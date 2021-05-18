f8c3801bbbcd005817e79b9e4b8c8764c9769150
pyros-cyber <simonepirota@gmail.com>
2021-05-18 22:42:32 +0200

    add discreteWalk calculation

--- exercise02/src/ex02.2.cpp
+++ exercise02/src/ex02.2.cpp
@@ -1,9 +1,49 @@
 #include "myStatFunc.h"
 #include "random.h"
+#include "randomWalk.h"
 #include <cmath>
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-int main(int argc, char *argv[]) { return 0; }
+int main(int argc, char *argv[]) {
+  // Variables
+  Random rnd("Primes", "seed.in");
+  unsigned int N = 100, M = 100000, steps = 100;
+  int throws_per_block = M / N;
+
+  /*************/
+  /*Exercise 02.2.1
+  simulate a 3d random walk on a discrete lattice
+  */
+  ofstream out("results02.2.discrete.dat");
+  ofstream out1("results02.2.discrete.error.dat");
+
+  if (out.is_open() && out.is_open()) {
+    for (int i{}; i < steps; i++) {
+      vector<double> r_sqDiscrete(N);
+      for (int j{}; j < N; j++) {
+        for (int k{}; k < throws_per_block; k++) {
+          shared_ptr<randomWalk> w = make_shared<DiscreteWalk>();
+          for (int l{}; l < i; l++) {
+            w->walk(make_shared<Random>(rnd));
+          }
+          r_sqDiscrete[j] += pow(w->distance_from_origin, 2);
+        }
+        r_sqDiscrete[j] /= static_cast<double>(throws_per_block);
+      }
+      vector<double> error = blocking_error(r_sqDiscrete);
+      out << i << " " << sqrt(r_sqDiscrete[N - 1]) << " " << error[N - 1]
+          << endl;
+      if (i == steps - 1) {
+        for (int j{}; j < N; j++) {
+          out1 << j << " " << r_sqDiscrete[j] << " " << error[j] << endl;
+        }
+      }
+    }
+  } else {
+    cerr << "ERROR: can't open output files" << endl;
+  }
+  return 0;
+}
