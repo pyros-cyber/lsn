ea930aa2d1ade92bad6e1fc826f162fd27beef2b
pyros-cyber <simonepirota@gmail.com>
2021-05-18 15:08:26 +0200

    add integrand definition

--- exercise02/src/ex02.1.cpp
+++ exercise02/src/ex02.1.cpp
@@ -1,9 +1,15 @@
 #include "myStatFunc.h"
 #include "random.h"
 #include <cmath>
+#include <cstdlib>
+#include <fstream>
 #include <iostream>
+#include <string>
 #include <vector>
 
 using namespace std;
 
+inline double integrand(double x) { return M_PI_2 * cos(M_PI_2 * x); }
+
+inline double non_unif_distr(double x) { return 0; }
 int main(int argc, char *argv[]) { return 0; }
