243a06bf9f36259165011223f3a04dd3ea6023a6
pyros-cyber <simonepirota@gmail.com>
2021-05-21 14:29:52 +0200

    add cli parser to main

--- exercise04/src/ex04.1.cpp
+++ exercise04/src/ex04.1.cpp
@@ -1,5 +1,37 @@
-#include <iostream>
+#include "MolDyn_NVE.h"
+#include "args.hxx"
 
 using namespace std;
 
-int main(int argc, char *argv[]) { return 0; }
+int interface(int argc, char *argv[]) {
+  args::ArgumentParser parser(
+      "This program performs a molecular dynamics simulation in NVE ensemble");
+  args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
+  args::ValueFlag<string> restart(
+      parser, "", "Start from configuration in old.0 and config.0",
+      {'r', "restart"});
+  args::ValueFlag<string> equilibrium(
+      parser, "", "Start from configuration in config.0", {'e', "equilibrium"});
+  args::ValueFlag<string> input(parser, "", "The input file", {'i', "input"});
+
+  try {
+    parser.ParseCLI(argc, argv);
+  } catch (args::Help) {
+    cout << parser;
+    return 0;
+  } catch (args::ParseError e) {
+    cerr << e.what() << std::endl;
+    cerr << parser;
+    return 1;
+  } catch (args::ValidationError e) {
+    cerr << e.what() << std::endl;
+    cerr << parser;
+    return 1;
+  }
+  return 0;
+}
+
+int main(int argc, char *argv[]) {
+  interface(argc, argv);
+  return 0;
+}
